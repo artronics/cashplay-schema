7a8bd2884836251d73f79fca615af1c7714e4763
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2017-01-02 22:04:12 +0000

    change seed and delete prev auth

--- db/utils/seed.sql
+++ db/utils/seed.sql
@@ -1,4 +1,4 @@
-INSERT INTO cashplay.customer (id, first_name, last_name, created_at) VALUES
+INSERT INTO cashplay.customers (id, first_name, last_name, created_at) VALUES
   (1, 'Sara', 'Powell', '2015-07-03T14:11:30Z'),
   (2, 'Andrea', 'Fox', '1999-04-04T21:21:42Z'),
   (3, 'Stephen', 'Banks', '2003-12-09T04:39:10Z'),
@@ -6,8 +6,3 @@ INSERT INTO cashplay.customer (id, first_name, last_name, created_at) VALUES
   (5, 'Kenneth', 'Williams', '2002-08-16T19:03:47Z'),
   (6, 'Ann', 'Peterson', '2013-09-24T15:05:29Z');
 
-ALTER SEQUENCE cashplay.customer_id_seq RESTART WITH 7;
-
-GRANT SELECT ON TABLE cashplay.customer to cashplay_anonymous;
-
-SELECT * FROM cashplay.register_person('jalal'::TEXT,'hosseini'::TEXT,'jalalhosseiny@gmail.com'::TEXT,'admin'::TEXT);
